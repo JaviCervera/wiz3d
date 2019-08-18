308f2ce094c4d7e5e3d99c84188d06d7b9ddcd4b
Javier San Juan Cervera <jedive@gmail.com>
2019-08-18 13:27:10 +0200

    .

---
+++ src/material_internal.h
@@ -0,0 +1,25 @@
+#ifndef MATERIAL_INTERNAL_H_INCLUDED
+#define MATERIAL_INTERNAL_H_INCLUDED
+
+#include "types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct SMaterial {
+  Texture* texture;
+  int diffuse;
+  int emissive;
+  int specular;
+  float shininess;
+  int blend;
+  int flags;
+  int shininesspower;
+} Material;
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* MATERIAL_INTERNAL_H_INCLUDED */
